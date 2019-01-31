2d4227383a93f7c3182215016eb94a9d2b8d6462
Isidro Lopez <islomar@spotify.com>
2019-01-31 21:33:20 +0100

    Include 'Backstage...'

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,7 +9,7 @@ public class GildedRoseTest {
   public void update_quality() throws Exception {
 
     CombinationApprovals.verifyAllCombinations(this::doUpdateQuality,
-        new String[]{ "foo", "Aged Brie" },
+        new String[]{ "foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" },
         new Integer[]{ 0 },
         new Integer[]{ 0 }
     );

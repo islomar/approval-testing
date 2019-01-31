525409052b14f8203d83066c661bbebff5ef90c0
Isidro Lopez <islomar@spotify.com>
2019-01-31 21:22:40 +0100

    Start using CombinationApprovals

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,22 +1,24 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 public class GildedRoseTest {
 
   @Test
-  public void update_quality() {
+  public void update_quality() throws Exception {
+    final String name = "foo";
     final int sellIn = 0;
     final int quality = 0;
-    final String name = "foo";
 
-    final String itemString = doUpdateQuality(sellIn, quality, name);
-
-    Approvals.verify(itemString);
+    CombinationApprovals.verifyAllCombinations(this::doUpdateQuality,
+        new String[]{ name },
+        new Integer[]{ sellIn },
+        new Integer[]{ quality }
+    );
   }
 
-  private String doUpdateQuality(final int sellIn, final int quality, final String name) {
+  private String doUpdateQuality(final String name, final int sellIn, final int quality) {
     Item[] items = new Item[]{ new Item(name, sellIn, quality) };
     GildedRose app = new GildedRose(items);
 

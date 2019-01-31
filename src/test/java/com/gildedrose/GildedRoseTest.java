63ec8da3f6d51c41113a321c97639b4ba424219f
Isidro Lopez <islomar@spotify.com>
2019-01-31 18:03:49 +0100

    Rename test method

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 public class GildedRoseTest {
 
   @Test
-  public void foo() {
+  public void update_quality() {
     Item[] items = new Item[]{ new Item("foo", 0, 0) };
     GildedRose app = new GildedRose(items);
-    
+
     app.updateQuality();
 
     assertEquals("foo", app.items[0].name);

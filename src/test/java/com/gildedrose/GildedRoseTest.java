7bb6ff3a3f39f5ff6c12b9748a2d9df8fb1bc2b2
Isidro Lopez <islomar@spotify.com>
2019-01-31 18:02:41 +0100

    Pass default test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,8 +10,10 @@ public class GildedRoseTest {
   public void foo() {
     Item[] items = new Item[]{ new Item("foo", 0, 0) };
     GildedRose app = new GildedRose(items);
+    
     app.updateQuality();
-    assertEquals("fixme", app.items[0].name);
+
+    assertEquals("foo", app.items[0].name);
   }
 
 }
